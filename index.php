0593ab399a0560826836da1f9f3aa20790f2b740
Neradence <neradence@gmail.com>
2018-04-27 09:43:46 +0300

    yoda condition

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-						<input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php echo $show_complete_tasks == 1 ? "checked" : ""; ?> >
+						<input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php echo (1 === $show_complete_tasks) ? "checked" : ""; ?> >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
